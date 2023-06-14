eaf983c2badc8004153f6336a7b3452103c3f421
Jonnas <jonnasmacaraeg@gmail.com>
2023-06-14 23:12:01 +0800

    Update

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,33 @@
 import { Text } from "@rneui/themed"
+import { useEffect, useState } from "react";
 import {View} from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
 
+const baseUrl = 'http://mdms.cypherph.com/';
 
 const HomeScreen = () =>{
+    const [nfcID,setNfcID]=useState('')
+    const [memberInfo,setMemberInfo]=useState({address:'',email:''});
+    useEffect(()=>{
+        getMemberById();
+    },[nfcID])
+    const getMemberById = async()=>{
+        const nfcID = await AsyncStorage.getItem('NFC_ID');
+        setNfcID(nfcID!);
+        const requestString = `${baseUrl}api/member/${nfcID}`
+        await axios.get(requestString)
+        .then((response)=>{
+            setMemberInfo(response.data);
+        })
+    }
     return(
         <View>
-            <Text>Welcome to NFC App</Text>
+            <Text>Welcome to NFC App {memberInfo.email}</Text>
         </View>
     )
 }
+
+
 
 export default HomeScreen
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,9 +1,58 @@
-import { Button, Image, Input, useTheme } from '@rneui/themed';
-import React from 'react';
+import { Button, Dialog, Image, Input, useTheme } from '@rneui/themed';
+import React, { useEffect, useState } from 'react';
 import {View, StyleSheet} from 'react-native';
 import { Text } from '@rneui/themed';
+import NfcManager, { Ndef, NfcEvents, NfcTech } from 'react-native-nfc-manager';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+
 const LoginScreen = ({navigation}:any) =>{
     const { theme } = useTheme();
+    const baseUrl = 'http://mdms.cypherph.com/';
+
+    const [isInvalidNFC,setIsInvalidNFC]=useState(false);
+
+    useEffect(() => {
+        startNfc();
+        () => NfcManager.cancelTechnologyRequest(); // unmount the scanner on navigate away.
+    },[navigation]);
+
+    
+    const startNfc = async () => {
+        try {
+            const supported = await NfcManager.isSupported();
+            const nfcScanning = await NfcManager.isEnabled();
+            if(!(supported && nfcScanning)){
+                console.warn("NFC Not workings")
+            }
+            if (supported && nfcScanning) {
+                console.warn("Started")
+                await NfcManager.start();
+                let tag: any;
+                await NfcManager.requestTechnology(NfcTech.Ndef);
+                tag = await NfcManager.getTag();
+                
+                if (tag) {
+                    tag.ndefStatus = await NfcManager.ndefHandler.getNdefStatus();
+                    const decodedData = Ndef.uri.decodePayload(tag.ndefMessage[0].payload);
+                await AsyncStorage.setItem('NFC_ID',decodedData.replace('https://www.en',''));
+                await apiCall(decodedData.replace('https://www.en',''));
+                }
+                NfcManager.cancelTechnologyRequest(); // Have to reset nfc scanner.
+                  };
+        } catch (error) {
+            NfcManager.cancelTechnologyRequest();
+        }
+    };
+
+    const apiCall = async(nfcID:string)=>{
+        const requestString = `${baseUrl}api/member/${nfcID}`
+        await axios.get(requestString)
+        .then((response)=>{
+            navigation.navigate('Home');
+        })
+    }
+
     return (
         <View style={styles.container}>
             <Image source={require('../assets/Logo.png')} containerStyle={styles.backgroundImage}></Image>
@@ -12,10 +61,20 @@ const LoginScreen = ({navigation}:any) =>{
             <Input label="Email"></Input>
             <Input label="Password"></Input>
             <Button 
-                onPress={() => navigation.navigate('Home')}
+                onPress={() => {
+                    navigation.navigate('Home');
+                }
+            }
                 buttonStyle={styles.button}
             >Sign In</Button>
             <Text style={{color:theme.colors.grey3,textAlign:'center'}}>Or Tap Your ID to Login</Text>
+
+            <Dialog
+                isVisible={isInvalidNFC}
+            >
+                <Dialog.Title title="Login"/>
+                <Text>Invalid NFC Card</Text>
+            </Dialog>
         </View>
     )
 }
